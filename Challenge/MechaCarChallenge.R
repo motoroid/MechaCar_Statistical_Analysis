49c4306434a193c5b25daf681be05d25fa1bcffc
motoroid <motoroid24@gmail.com>
2023-02-27 15:37:11 -0700

    finished R coding for challenge

--- Challenge/MechaCarChallenge.R
+++ Challenge/MechaCarChallenge.R
@@ -12,11 +12,27 @@ total_summary<-summarize(suspension_table,
                          Median=median(suspension_table$PSI),
                          Variance = var(suspension_table$PSI),
                          SD=sd(suspension_table$PSI))
+#summary by Lot
 lot_summary<- suspension_table%>%group_by(Manufacturing_Lot) %>% 
 summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI), Std_Dev = sd(PSI), .groups='keep')
 
+#t.test to determine if PSI across all lots is different than 1,500 lbs/inch^2
+t.test(suspension_table$PSI, mu=1500)
+
+#t.test and subset to determine if PSI across each lot is different than 1,500 lbs/inch^2
+lot1<-subset(suspension_table, Manufacturing_Lot =="Lot1")
+lot2<-subset(suspension_table, Manufacturing_Lot =="Lot2")
+lot3<-subset(suspension_table, Manufacturing_Lot =="Lot3")
+
+t.test(lot1$PSI,suspension_table$PSI)
+t.test(lot2$PSI,suspension_table$PSI)
+t.test(lot3$PSI,suspension_table$PSI)
 
 
+
+
+
+#extra code not needed
 
 #linear regression all separate
 
